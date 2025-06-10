be60598c9967009cb588a20a78b4b46853d67f9b
arsiddiqui <siddiqui.ashar@gmail.com>
2025-06-09 23:08:49 -0500

    Structure

---
+++ pishingAnalyzer/pishing_analyzer.py
@@ -0,0 +1,22 @@
+#Developer: Ashar Siddiqui
+#Date Created: 06/09/2025
+#Date Updated: 06/09/2025
+#Email Scanner
+#Change Log
+#CSC 842, Security tool development Cycle 5
+
+
+
+
+
+
+
+
+def main():
+
+
+
+
+
+if __name__ == "__main__":
+    main()
